9b6d4e4814df537958fd6a76b3528bc40ea4b186
Kulraj Singh Kohli <kulraj0000@gmail.com>
2021-07-12 15:50:45 -0700

    Debugging

--- CSV to JSON Pipeline/Code/CSVtoJSON_pipeline.py
+++ CSV to JSON Pipeline/Code/CSVtoJSON_pipeline.py
@@ -1,6 +1,6 @@
 # Libraries to schedule the DAG
 
-import datetime as datetime
+import datetime as dt
 from datetime import timedelta
 
 # Libraries to build the DAG using Bash and Python Operators
@@ -26,7 +26,7 @@ def CSVToJSON():
 
 default_args = {
 	'owner':'KSK',
-	'start_date': dt.datetime(2021,07,11)
+	'start_date': dt.datetime(2021,7,11),
 	'retries':1,
 	'retry_delay':dt.timedelta(minutes=5)
 }
@@ -39,21 +39,22 @@ with DAG('MyCSVDAG',
 	schedule_interval = timedelta(minutes=5),
 	#'0****',
 	) as dag:
+	#Task: printing out a statement using bash
+
+	print_starting = BashOperator(task_id='starting',bash_command='echo"I am reading the CSV now....."')
+
+	#Task: call funtion defined earlier
+
+	CSVJson= PythonOperator(task_id='convertCSVtoJson',python_callable=CSVToJSON)
+
+	# Making connection between tasks
+
+	print_starting.set_downstream(CSVJson) # also can do print_starting >> CSVJson
+
+	CSVJson.set_upstream(print_starting) # also can do CSVJson << print_starting
 
 
-#Task: printing out a statement using bash
 
-print_starting = BashOperator(task_id='starting',bash_command='echo"I am reading the CSV now....."')
-
-#Task: call funtion defined earlier
-
-CSVJson= PythonOperator(task_id='convertCSVtoJson',python_callable=CSVToJSON)
-
-# Making connection between tasks
-
-print_starting.set_downstream(CSVJson) # also can do print_starting >> CSVJson
-
-CSVJson.set_upstream(print_starting) # also can do CSVJson << print_starting
 
 
 
